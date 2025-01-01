a8e56724697e358f5f580e70b1747a76f44bdd82
lirovsk <joao.lira@engenharia.ufjf.br>
2025-01-01 15:41:45 -0300

    Settin up the gaming enviroment to test the jogo's class functions

--- main.py
+++ main.py
@@ -48,6 +48,7 @@ class jogo:
             for i in range(0, leader_choice):
                 self.print_and_save_options(leader, self.list_options, list_restricted, chosed)
             print ( "Os selecionados para a missão foram: ",  chosed)
+            return chosed
 
         elif round == 2:
             leader_choice = 3
@@ -56,6 +57,7 @@ class jogo:
             for i in range(0, leader_choice):
                 self.print_and_save_options(leader, self.list_options, list_restricted, chosed)
             print ( "Os selecionados para a missão foram: ",  chosed)
+            return chosed
 
         elif round == 3:
             list_restricted = []
@@ -64,6 +66,7 @@ class jogo:
             for i in range(0, leader_choice):
                 self.print_and_save_options(leader, self.list_options, list_restricted, chosed)
             print ( "Os selecionados para a missão foram: ",  chosed)
+            return chosed
 
         elif round == 4:
             leader_choice = 4
@@ -72,6 +75,7 @@ class jogo:
             for i in range(0, leader_choice):
                 self.print_and_save_options(leader, self.list_options, list_restricted, chosed)
             print ( "Os selecionados para a missão foram: ",  chosed)
+            return chosed
 
         elif round == 5:
             leader_choice = 4
@@ -80,6 +84,7 @@ class jogo:
             for i in range(0, leader_choice):
                 self.print_and_save_options(leader, self.list_options, list_restricted, chosed)
             print ( "Os selecionados para a missão foram: ",  chosed)
+            return chosed
 
     
     def list_options(self):
@@ -126,6 +131,49 @@ class jogo:
             final_aproval = False
         return final_aproval
 
+    def incursion_success (self, list_of_players, round):
+        List_result = []
+        positive = 0
+        negative = 0
+        positive_awnser = ["sim", "ok", "s", "y"]
+        negative_awnser = ["não", "nao", "n", "nop"]
+        if round in [1,2,3,5]:
+            for i in list_of_players:
+                print(f"{list_of_players[i]}, deseja sabotar essa missão?")
+                result_incurssoin = input()
+                if result_incurssoin in positive_awnser:
+                    List_result.extend(1)
+                elif result_incurssoin in negative_awnser:
+                    List_result.extend(0)
+                for i in List_result:
+                    if List_result[i] == 1:
+                        positive += 1
+                    else:
+                        negative += 1
+                if negative > 0:
+                    return False
+                else:
+                    return True
+        else:
+            for i in list_of_players:
+                print(f"{list_of_players[i]}, deseja sabotar essa missão?")
+                result_incurssoin = input()
+                if result_incurssoin in positive_awnser:
+                    List_result.extend(1)
+                elif result_incurssoin in negative_awnser:
+                    List_result.extend(0)
+                for i in List_result:
+                    if List_result[i] == 1:
+                        positive += 1
+                    else:
+                        negative += 1
+                if negative >= 2:
+                    return False
+                else:
+                    return True
+
+
+
 #Begining of the code/game
 TEXTO_1 = """"
                 .___________. __    __   _______ 
@@ -145,24 +193,47 @@ TEXTO_2 = """
 TEXTO_3 = """Bem vindo ao jogo de estratégia e espioangem para ser jogado em grupo!
                          Aperte qualquer tecla para continuar"""
 TEXTO_4 = "Insira o núnero de jogadores da partida: "
-
+TEXTO_5 = """O jogo está prestes a começar, atenção aos seu arredores! A pessoa ao seu lado pode ser um espião"""
 print(TEXTO_1, "\n", TEXTO_2)
 input(TEXTO_3)
 numero_players=input(TEXTO_4)
+positive_awnser = ["sim", "ok", "s", "y"]
+negative_awnser = ["não", "nao", "n", "nop"]
 
 jogo1 = jogo(numero_players)
-round_aux = 0
-time_player = randint(0,6)
-keep_playing = True
-jogo1.show_function
-rejections = 0
-while keep_playing:
-    jogo1.leader_order(time_player, round_aux)
-    result = jogo1.mission_aproval()
-    if result:
-        round_aux +=1
-        #função de missão
-    else:
-        rejections += 1
-    
+same_game = True
+
+while same_game:
+    print(TEXTO_5)
+    input("(Aperte Enter)")
+    round_aux = 1
+    time_player = randint(0,6)
+    keep_playing = True
+    rejections = 0
+    resistence_points = 0
+    spy_points = 0
+    jogo1.show_function()
+    while keep_playing:
+        jogadores_selecionados = jogo1.leader_order(time_player, round_aux)
+        result = jogo1.mission_aproval()
+        if result:
+            incursion_result = jogo1.incursion_success(result, round_aux)
+            round_aux +=1
+            if incursion_result:
+               resistence_points += 1
+            else:
+                spy_points += 1
+        else:
+            rejections += 1
+        if (rejections == 6) or (spy_points == 3):
+            print("Os espiões ganharam o jogo!")
+            break
+        if (resistence_points == 3):
+            print("A resitência ganhou o jogo")
+            break
+    print("Você deseja continuar jogando com as mesmas pesoas?")
+    if input() in positive_awnser:
+        continue
+    if input in negative_awnser:
+        break
     
